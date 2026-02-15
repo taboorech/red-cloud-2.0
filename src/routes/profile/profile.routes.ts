2b8041b2e43ac581de636caeb7bfa8e7249291ad
taboorech <karuselpopka@gmail.com>
2026-02-15 22:56:04 +0200

    feat: add avatar upload and deletion in profile update

--- src/routes/profile/profile.routes.ts
+++ src/routes/profile/profile.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { Container } from "inversify";
 import ProfleController from "./profile.controller";
+import multer from "multer";
+import { multerStorage } from "@app/lib/utils/multer";
 
 const createProfileRoutes = (ioc: Container): Router => {
   const router = Router();
 
   const ctrl = ioc.get(ProfleController);
 
+  const upload = multer({ storage: multerStorage });
+
   router.get("/", ctrl.getProfile);
-  router.put("/", ctrl.updateProfile);
+  router.put("/", upload.single("avatar"), ctrl.updateProfile);
   router.put("/password", ctrl.changeUserPassword);
 
   return router;
